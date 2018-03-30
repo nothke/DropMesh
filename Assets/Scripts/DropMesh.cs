de9fa73d4a8f657945bc2ac4a997efebb1ed17b0
Ivan Notaros <nothke@gmail.com>
2018-03-30 10:04:13 +0200

    Drops working for the first time

--- Assets/Scripts/DropMesh.cs
+++ Assets/Scripts/DropMesh.cs
@@ -9,10 +9,10 @@ public class DropMesh : MonoBehaviour
 
     TMesh tmesh;
 
+    public int width = 8;
+
     void Start()
     {
-        int width = 3;
-
         Vector3[] points = new Vector3[width * width];
 
         for (int x = 0; x < width; x++)
@@ -31,24 +31,47 @@ public class DropMesh : MonoBehaviour
     {
         List<int> tris = new List<int>();
 
-        // in the first column we will drop one
+        int firstRowIncrement = 1;
+        int secondRowIncrement = 2;
+
         for (int x = 0; x < width - 1; x++)
         {
-            int firstRowIncrement = 1;
-            int secondRowIncrement = 1;
+            int y = 0;
 
-            for (int y = 0; y < width / 2; y += 2)
+            bool incomplete = false;
+
+            while (y < width)
             {
+                int toEnd = width - y - 1;
+
+                if (toEnd <= firstRowIncrement)
+                {
+                    Debug.Log(incomplete ? "Incomplete" : "Final");
+                    break; // break for now
+                }
+
                 // first row
                 int a = x * width + y;
-                int b = x * width + y + 1;
-                int c = x * width + y + 2;
+                int b = x * width + y + firstRowIncrement;
+                int c = x * width + y + firstRowIncrement * 2;
 
                 // second row
                 int d = (x + 1) * width + y;
-                int e = (x + 1) * width + y + 2; // here is the skipped one
+                int e = (x + 1) * width + y + secondRowIncrement; // here is the skipped one
+
+                if (y + secondRowIncrement >= width) e = (x + 1) * width + width - 1;
+                if (c != e) incomplete = true;
 
                 TriangleFan(ref tris, a, b, c, d, e); // we add triangles to the list
+
+                y += secondRowIncrement;
+            }
+
+            if (x < 1)
+            {
+
+                firstRowIncrement = secondRowIncrement;
+                secondRowIncrement = firstRowIncrement * 2;
             }
         }
 
@@ -66,7 +89,7 @@ public class DropMesh : MonoBehaviour
 
     void TriangleFan(ref List<int> tris, int a, int b, int c, int d, int e)
     {
-        Debug.Log(string.Format("Making tris from: {0}, {1}, {2}, {3}, {4}", a, b, c, d, e));
+        //Debug.Log(string.Format("Making tris from: {0}, {1}, {2}, {3}, {4}", a, b, c, d, e));
 
         tris.Add(a); tris.Add(d); tris.Add(b);
         tris.Add(b); tris.Add(d); tris.Add(e);
