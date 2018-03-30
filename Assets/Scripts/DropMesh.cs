c7f4cc3e7e98a57134a9e30a8885cc783d1fcd1b
Ivan Notaros <nothke@gmail.com>
2018-03-30 13:22:11 +0200

    The new method with discarded vertices is totally working

--- Assets/Scripts/DropMesh.cs
+++ Assets/Scripts/DropMesh.cs
@@ -20,17 +20,122 @@ public class DropMesh : MonoBehaviour
         {
             for (int y = 0; y < length; y++)
             {
-
-                float height = Mathf.PerlinNoise(x * 0.234f, y * 0.234f);
+                float height = 0;// Mathf.PerlinNoise(x * 0.234f, y * 0.234f);
                 points[x * length + y] = new Vector3(x * x * 2, height, y);
-                
             }
         }
 
-        TMesh tmesh = Simplify(points, width, length);
+        TMesh tmesh = Simplify2(points, width, length);
         Mesh mesh = tmesh.ToMesh();
         mesh.RecalculateNormals();
         gameObject.InitializeMesh(mesh);
+    }
+
+    TMesh Simplify2(Vector3[] vertices, int width, int length)
+    {
+        List<int> tris = new List<int>();
+        List<Vector3> droppedVerts = new List<Vector3>();
+
+        int rowIncrement = 1;
+
+        for (int x = 0; x < width; x++)
+        {
+            rowIncrement = GetRowIncrement(x);
+
+            for (int y = 0; y < length; y++)
+            {
+                if (y == (length - 1) || (y % rowIncrement) == 0)
+                    droppedVerts.Add(vertices[x * length + y]);
+            }
+        }
+
+        // Debug these dropped verts
+        for (int i = 0; i < droppedVerts.Count; i++)
+        {
+            Debug.DrawRay(droppedVerts[i], Vector3.up, Color.red, 1);
+        }
+
+        // TRIANGLES
+
+        int a, b, c, d, e;
+
+        int thisRowIncrement = GetRowIncrement(0);
+        int nextRowIncrement;
+
+        int thisRowCount = GetRowCount(0, thisRowIncrement);
+        int nextRowCount;
+
+        int thisRow0 = 0;
+        int nextRow0;
+
+        for (int x = 0; x < width - 1; x++)
+        {
+            nextRowIncrement = GetRowIncrement(x + 1);
+            nextRowCount = GetRowCount(x + 1, nextRowIncrement);
+            nextRow0 = thisRow0 + thisRowCount;
+
+            // Just output row counts for now
+            Debug.Log(thisRowCount);
+            Debug.Log("nextRow0 " + nextRow0);
+
+            int thisRowV = thisRow0;
+            int nextRowV = nextRow0;
+
+            while (true)
+            {
+                a = thisRowV;
+                b = thisRowV + 1;
+                c = thisRowV + 2;
+
+                d = nextRowV;
+                e = nextRowV + 1;
+
+                // End condition
+                if (c >= nextRow0)
+                {
+                    // If the piece cannot be dropped, make a quad
+                    if (b - thisRow0 < thisRowCount)
+                    {
+                        Debug.Log("Quad situation");
+                        Triangulate(ref tris, a, b, d, e);
+                        break;
+                    }
+                    else
+                        break;
+                }
+
+                Triangulate(ref tris, a, b, c, d, e);
+
+                thisRowV += 2;
+                nextRowV += 1;
+            }
+
+            thisRowIncrement = nextRowIncrement;
+            thisRowCount = nextRowCount;
+            thisRow0 = nextRow0;
+        }
+
+        // Finalize
+        TMesh tmesh = new TMesh();
+        tmesh.vertices = droppedVerts.ToArray();
+        tmesh.triangles = tris.ToArray();
+        return tmesh;
+    }
+
+    int GetRowIncrement(int row)
+    {
+        return (int)Mathf.Pow(2, row);
+    }
+
+    int GetRowCount(int row, int increment)
+    {
+        int count = 0;
+
+        for (int y = 0; y < length; y++)
+            if (y == (length - 1) || (y % increment) == 0)
+                count++;
+
+        return count;
     }
 
     TMesh Simplify(Vector3[] vertices, int width, int length)
@@ -122,7 +227,7 @@ public class DropMesh : MonoBehaviour
 
     void Triangulate(ref List<int> tris, int a, int b, int c, int d, int e)
     {
-        Debug.Log(string.Format("Making tris from: {0}, {1}, {2}, {3}, {4}", a, b, c, d, e));
+        //Debug.Log(string.Format("Making tris from: {0}, {1}, {2}, {3}, {4}", a, b, c, d, e));
 
         tris.Add(a); tris.Add(b); tris.Add(d);
         tris.Add(b); tris.Add(e); tris.Add(d);
