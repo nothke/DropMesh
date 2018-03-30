9c26fea79e242db3b64886a3e5bff53bc723188b
Ivan Notaros <nothke@gmail.com>
2018-03-30 15:10:33 +0200

    Removed the old method that didn't reduce vertex count

--- Assets/Scripts/DropMesh.cs
+++ Assets/Scripts/DropMesh.cs
@@ -6,7 +6,6 @@ using MeshXtensions;
 
 public class DropMesh : MonoBehaviour
 {
-
     TMesh tmesh;
 
     public int width = 8;
@@ -20,18 +19,18 @@ public class DropMesh : MonoBehaviour
         {
             for (int y = 0; y < length; y++)
             {
-                float height = 0;// Mathf.PerlinNoise(x * 0.234f, y * 0.234f);
+                float height = Mathf.PerlinNoise(x * 0.234f, y * 0.234f) * 10;
                 points[x * length + y] = new Vector3(x * x * 2, height, y);
             }
         }
 
-        TMesh tmesh = Simplify2(points, width, length);
+        TMesh tmesh = Simplify(points, width, length);
         Mesh mesh = tmesh.ToMesh();
         mesh.RecalculateNormals();
         gameObject.InitializeMesh(mesh);
     }
 
-    TMesh Simplify2(Vector3[] vertices, int width, int length)
+    TMesh Simplify(Vector3[] vertices, int width, int length)
     {
         List<int> tris = new List<int>();
         List<Vector3> droppedVerts = new List<Vector3>();
@@ -136,87 +135,6 @@ public class DropMesh : MonoBehaviour
                 count++;
 
         return count;
-    }
-
-    TMesh Simplify(Vector3[] vertices, int width, int length)
-    {
-        List<int> tris = new List<int>();
-
-        int firstRowIncrement = 1;
-        int secondRowIncrement = 2;
-
-        int a, b, c, d, e;
-
-        for (int x = 0; x < width - 1; x++)
-        {
-            int y = 0;
-
-            bool incomplete = false;
-
-            while (true)
-            {
-                int toEnd = length - y - 1;
-
-                // Ending
-                if (toEnd <= firstRowIncrement)
-                {
-                    Debug.Log(incomplete ? "Incomplete" : "Final");
-
-                    if (!incomplete) // in case the row ends with a quad
-                    {
-                        a = x * length + y;
-                        b = x * length + length - 1;
-
-                        c = (x + 1) * length + y;
-                        d = (x + 1) * length + length - 1;
-
-                        Triangulate(ref tris, a, b, c, d);
-                    }
-                    else // in case the row ends with a triangle
-                    {
-                        y = y - secondRowIncrement + firstRowIncrement;
-
-                        Debug.Log(y);
-
-                        a = x * length + y;
-                        b = x * length + length - 1;
-                        c = (x + 1) * length + length - 1;
-
-                        Debug.Log(string.Format("ABC: {0}, {1}, {2}", a, b, c));
-                        tris.Add(a); tris.Add(b); tris.Add(c);
-                    }
-
-                    break;
-                }
-
-                // first row
-                a = x * length + y;
-                b = x * length + y + firstRowIncrement;
-                c = x * length + y + firstRowIncrement * 2;
-
-                // second row
-                d = (x + 1) * length + y;
-                e = (x + 1) * length + y + secondRowIncrement; // here is the skipped one
-
-                if (y + secondRowIncrement >= length) e = (x + 1) * length + length - 1;
-                if (c != e - length) incomplete = true;
-
-                Triangulate(ref tris, a, b, c, d, e); // we add triangles to the list
-
-                y += secondRowIncrement;
-            }
-
-            Debug.Log("Row END");
-
-            firstRowIncrement = secondRowIncrement;
-            secondRowIncrement = firstRowIncrement * 2;
-
-        }
-
-        TMesh tmesh = new TMesh();
-        tmesh.vertices = vertices;
-        tmesh.triangles = tris.ToArray();
-        return tmesh;
     }
 
     void Triangulate(ref List<int> tris, int a, int b, int c, int d)
